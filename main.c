219a4e3de6136522ef8b397ab84a19897ca8ccc4
jetamkadlec <kadlecm6@gmail.com>
2016-02-24 13:06:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void main()
 {
     int MatchesLeft = 21;
     int ToRemove = 0;
     while(MatchesLeft>1)
-    { //1st player´s turn
+    { //1st playerÂ´s turn
     printf("%d matches remaining\n", MatchesLeft);
     printf("Player 1, how many matches do you want to remove?(1,2 or 3)\n");
     scanf("%d", &ToRemove); //player 1 decides how many matches to remove
@@ -16,7 +16,7 @@ int main()
         printf("You lose, player 1"); //win check
     }
     else
-    {     //2nd player´s turn
+    {     //2nd playerÂ´s turn
         int ToRemove = 0;
          printf("%d matches remaining\n", MatchesLeft);
         printf("Player 2, how many matches do you want to remove?(1,2 or 3)\n");
